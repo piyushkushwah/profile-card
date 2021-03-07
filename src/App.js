e27e88204601dac650d67e0b263707bbe8e6f0d0
Piyush Kushwah <kushwahpiyush4@gmail.com>
2021-03-07 21:16:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import top from './assets/bg-pattern-top.svg';
+import bottom from './assets/bg-pattern-bottom.svg';
+import card from './assets/bg-pattern-card.svg';
+import victor from './assets/image-victor.jpg';
 
 function App() {
+
+  let socialData = [
+
+    {
+      count: '80K',
+      name: 'Followers'
+    },
+
+    {
+      count: '803K',
+      name: 'Likes'
+    },
+
+    {
+      count: '1.4K',
+      name: 'Photos'
+    }
+
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <img className="top-bg"
+        src={top} alt="top" loading="lazy" />
+
+      <div className="profile-card">
+
+        <div className="card-pattern">
+          <img src={card} alt="image" />
+        </div>
+
+        <div className="details">
+
+          <div className="profile-image-wrapper">
+            <img src={victor} alt="profile" />
+          </div>
+
+          <div className="user-name">
+            <h1>Victor Crest <span>26</span></h1>
+            <p style={{ marginTop: '10px' }}>London</p>
+          </div>
+
+          <div className="social-data-wrapper">
+            {
+              socialData.map((e, index) => {
+                return (
+                  <div style={{
+                    width: '100%',
+                    textAlign: 'center'
+                  }}>
+                    <h1>{e.count}</h1>
+                    <p>{e.name}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+        </div>
+
+      </div>
+
+      <img className="bottom-bg"
+        src={bottom} alt="bottom" loading="lazy" />
+
+    </div >
   );
 }
 
